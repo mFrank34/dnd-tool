7dd4036e35b1aed01aea399bdc93a8be49cb9e10
Michael Frank <mffranks34@gmail.com>
2026-06-21 14:35:54 +0100

    adding dice roller class

---
+++ src/dice.h
@@ -0,0 +1,19 @@
+/*
+   dice.h  
+   Author: Frank <mffranks34@gmail.com>
+   Created:  21/06/2026
+   Updated:  21/06/2026
+   */
+
+#include <random>
+
+class Dice 
+{
+    private:
+        std::random_device rd;
+        std::mt19937 gen(rd());
+    public:
+
+
+}
+
